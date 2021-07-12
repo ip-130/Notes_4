c139c2e718bde0efa66a56e0aba9bcb8b3bc61d9
user_astr <ip-130@mail.ru>
2021-07-12 11:18:46 +0300

    Добавил метод, который автоматически ставит дату в строке заметки

--- app/src/main/java/com/android/notes_4/Fragment_two.java
+++ app/src/main/java/com/android/notes_4/Fragment_two.java
@@ -1,15 +1,26 @@
 package com.android.notes_4;
 
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
 
 public class Fragment_two extends Fragment {
     private static final String ARG_INDEX = "index";
     private int index;
+
+    TextView tvNoteDate;
+    EditText etText;
+    EditText etTitle;
+    Calendar dateAndTime=Calendar.getInstance();
 
     public static Fragment_two newInstance(int index) {
         Fragment_two f = new Fragment_two();    // создание
@@ -34,6 +45,22 @@ public class Fragment_two extends Fragment {
                              Bundle savedInstanceState) {
         // Таким способом можно получить головной элемент из макета
         View view = inflater.inflate(R.layout.fragment_two, container, false);
+        initView(view);
+        setInitialDateTime();
         return view;
+    }
+
+    //Метод получение пользовательских элементов по идентификатору
+    public void initView(View view){
+        etText = (EditText)view.findViewById(R.id.etText);
+        tvNoteDate = (TextView)view.findViewById(R.id.tvNoteDate);
+        etTitle = (EditText)view.findViewById(R.id.etTitle);
+    }
+
+    private void setInitialDateTime() {
+        tvNoteDate.setText(DateUtils.formatDateTime(getContext(),
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
     }
 }
